f8be107341aa37cd4d1f0ca7037142d21e89229f
Nikola Pestanac <43499406+nikolap3@users.noreply.github.com>
2019-01-09 19:19:55 +0100

    Revert "Branch"

---
+++ Database1/Scripts/ScriptsIgnoredOnImport.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE TABLE IF NOT EXISTS `geonodes`.`mesta` (
+  `id` VARCHAR(255) NULL DEFAULT NULL,
+  `longtitude` FLOAT NULL DEFAULT NULL,
+  `Latitude` FLOAT NULL DEFAULT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = ascii
+GO
+
+--Syntax Error: Incorrect syntax near `.
+--CREATE TABLE IF NOT EXISTS `geonodes`.`mesta` (
+--  `id` VARCHAR(255) NULL DEFAULT NULL,
+--  `longtitude` FLOAT NULL DEFAULT NULL,
+--  `Latitude` FLOAT NULL DEFAULT NULL)
+--ENGINE = InnoDB
+--DEFAULT CHARACTER SET = ascii
+
+
+
+GO
--- todoapi/Models/TodoItem.cs
+++ todoapi/Models/TodoItem.cs
@@ -9,7 +9,6 @@ namespace TodoApi.Models
     {
         public long Id { get; set; }
         public string Name { get; set; }
-        public float longtitude { get; set; }
-        public float latitude { get; set; }
+        public bool IsComplete { get; set; }
     }
 }
